e43e5ff02c4b5ddbdb5030daa2e65cf41c130c60
John <johndetora@gmail.com>
2021-07-01 15:21:46 -0400

    colors don't work

--- src/components/ExportExcel.js
+++ src/components/ExportExcel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as XLSX from 'xlsx';
-
+import '../App.css';
 const ExportExcel = props => {
     let data = props.data;
     // https://www.youtube.com/watch?v=tKz_ryychBY&list=PLQ9Y_YUSECCg0uiQR-Uj8E2a8fxTelcNB&index=3&t=1197s
