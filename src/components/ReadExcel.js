8284b9b6a84e40c8631da2daaa4f5fbae20e0307
John <johndetora@gmail.com>
2021-07-14 14:38:35 -0400

    word

--- src/components/ReadExcel.js
+++ src/components/ReadExcel.js
@@ -7,15 +7,21 @@ function ExcelReader(props) {
         const fileReader = new FileReader();
         fileReader.readAsArrayBuffer(file);
         fileReader.onload = e => {
+            // Set variables
             const sheetName = prompt('Please enter the name of the sheet');
             const bufferArray = e.target.result;
             const wb = XLSX.read(bufferArray, { type: 'buffer' });
 
             const worksheet = wb.Sheets[sheetName];
+            console.log('worksheet', worksheet);
             const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+            console.group();
+            console.table('data', data);
             const sysexMatch = /F0(.*?)F7/gm;
             let sheetObj = [];
-            for (let i = 0; i < data.length; i++) {
+            const MAX = data.length;
+
+            for (let i = 0; i < MAX; i++) {
                 sheetObj.push({ index: i, test: data[i][0], description: data[i][1], expected: data[i][2], sysex: null, result: '' });
             }
             // Assign sysex property to regexed sysex
@@ -25,16 +31,15 @@ function ExcelReader(props) {
                     sheetObj[i].sysex = sheetObj[i].description.match(sysexMatch);
                 }
             }
+
+            console.table('sheet object', sheetObj);
             // Filter out all rows that don't include a valid sysex cell
             const onlySysex = sheetObj.filter(cell => cell.sysex !== null);
-
             // Reassign index
             for (let i = 0; i < onlySysex.length; i++) {
                 onlySysex[i].index = i;
             }
-
             props.setItems(onlySysex);
-
             console.log('Worksheet load successful');
         };
     }
